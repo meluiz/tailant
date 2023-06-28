ecb2ae559d33b5d6bcfefe30244d3a1d82e085e9
meluiz <me.luizfelipe@gmail.com>
2023-06-27 23:53:58 -0300

    Initial commit from developer

---
+++ tsup.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig((options) => ({
+  dts: true,
+  clean: true,
+  outDir: './dist',
+  splitting: false,
+  sourcemap: false,
+  minify: !options.watch,
+  format: ['cjs', 'esm'],
+  entry: ['src/**/*.ts'],
+  tsconfig: './tsconfig.json',
+}))
